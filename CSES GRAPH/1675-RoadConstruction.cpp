664021168b7cd523ee4d2ad03211bb8e9fe21a24
AJAY KUMAR <54741067+manitajay@users.noreply.github.com>
2022-06-09 12:34:44 +0530

    Create 1675-RoadConstruction.cpp
    basically it is prim's algo :
    we can implement it if we have knowledge of dijkastra even if we don't know about prim's algo.
    https://cp-algorithms.com/graph/mst_prim.html

---
+++ CSES GRAPH/1675-RoadConstruction.cpp
@@ -0,0 +1,76 @@
+// JAI BAJARANG BALI
+
+// manitianajay45
+
+// give me some sunshine, give me some rain , give me another chance to grow up once again....
+
+// sab moh maya hai....
+
+
+/*
+basically it is prim's algo :
+we can implement it if we have knowledge of dijkastra even if we don't know about prim's algo...
+https://cp-algorithms.com/graph/mst_prim.html
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define mod 1000000007
+ll n,m;
+
+
+vector<pair<ll,ll>> graph[200005];
+vector<bool> visited(200005, false);
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // ll n;
+    cin >> n>>m;
+
+    for (ll i = 0; i <m; i++)
+    {
+        ll u, v,c;
+        cin >> u >> v>>c;
+        graph[u].push_back({v,c});
+        graph[v].push_back({u,c});
+        
+    }   
+    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>> pq;
+    pq.push({0,1});
+    ll ans=0;
+    while(!pq.empty()){
+        pair<ll,ll> pr=pq.top();
+        pq.pop();
+
+        if(visited[pr.second]){
+            continue;
+        }
+        visited[pr.second]=true;
+        ans+=pr.first;
+
+        for(auto i:graph[pr.second]){
+            if(!visited[i.first]){
+                pq.push({i.second,i.first});
+            }
+        }
+    }
+    for(ll i=1;i<=n;i++){
+        if(!visited[i]){
+            cout<<"IMPOSSIBLE"<<endl;
+            return 0;
+        }
+    }
+    cout<<ans<<endl;
+
+
+
+    return 0;
+}
