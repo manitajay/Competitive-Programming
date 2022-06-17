0c9e6d03d75f84d1979b65014d54d1e599a83c33
AJAY KUMAR <54741067+manitajay@users.noreply.github.com>
2022-06-17 22:02:48 +0530

    Create B.cpp

---
+++ BINARY SEARCH/step1/B.cpp
@@ -0,0 +1,56 @@
+// JAI BAJARANG BALI
+ 
+// manitianajay45
+ 
+// sab moh maya hai..
+ 
+// give me some sunshine, give me some rain, give me another chance to grow up once again
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define ll long long
+ 
+ll binarysearch(ll arr[],ll l,ll r,ll val)
+{
+   
+    
+    ll mid=(l+r)/2;
+    
+    if(r-l==1  && arr[mid]<=val)
+    {
+        return r;
+    }
+    if(r-l==1 && arr[mid]>val)
+    {
+        return 0;
+    }
+    if(arr[mid]>val)
+    {
+        return binarysearch(arr,l,mid,val);
+    }else if(arr[mid]<=val)
+    {
+        return binarysearch(arr,mid,r,val);
+    }
+    return 0;
+    
+}
+ 
+int main()
+{
+    ll n,k;
+    cin>>n>>k;
+    ll arr[n];
+    for(ll i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    while(k--)
+    {
+        ll val;
+        cin>>val;
+        cout<<binarysearch(arr,-1,n,val)<<endl;
+        
+    }
+    return 0;
+}
