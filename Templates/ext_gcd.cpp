76864a483d0e6adb3a89842cb53eeb291d38f853
Ajay Bhakar <54741067+manitajay@users.noreply.github.com>
2023-08-23 21:06:42 +0530

    Create ext_gcd.cpp

---
+++ Templates/ext_gcd.cpp
@@ -0,0 +1,14 @@
+
+ll ext_gcd(ll a, ll b, ll& x, ll& y) 
+{
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll d = gcd(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - y1 * (a / b);
+    return d;
+}
